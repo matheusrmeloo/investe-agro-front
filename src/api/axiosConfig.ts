52aaa05798f686ada5b1e0595767c0ae30db5a6d
Matheus Melo <matheus.melo@trademaster.com.br>
2025-01-04 13:54:43 -0300

    Adding new config to env and index html

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL:
+    process.env.REACT_APP_BACKEND_URL ||
+    'https://investe-agro-api-production.up.railway.app',
 });
 
 export default api;
